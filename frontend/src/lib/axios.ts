0bde9565ad8cbd67e5aa876aed197e0a591a4566
Giorgi Sakhelashvili <135421822+SakhelaTheInvincible@users.noreply.github.com>
2025-06-16 15:36:36 +0400

    Delete frontend directory

--- frontend/src/lib/axios.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'http://localhost:8000/api', // Your Django backend URL
-  timeout: 1000,
-  headers: { 'Content-Type': 'application/json' },
-});
-
-export default instance; 
