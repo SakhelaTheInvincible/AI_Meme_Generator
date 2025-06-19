78bee8a263dc0f2309363612a56c0bf9bd77f66a
SabaBeradze <113034681+SabaBeradze@users.noreply.github.com>
2025-06-19 18:46:27 +0400

    mc

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from '@/components/Header';
+import { AuthProvider } from '@/context/AuthContext';
 
 
 const geistSans = Geist({
@@ -28,8 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-
+        <AuthProvider>
+          <Header />
           <main>{children}</main>
+        </AuthProvider>
       </body>
     </html>
   );
