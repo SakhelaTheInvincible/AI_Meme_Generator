68d0e6c5327f36f9d9cd4acdf6b44900ec0291ec
Gigi Jishkariani <100708541+GigiJishkariani@users.noreply.github.com>
2025-06-23 13:20:50 +0400

    updated settings for image upload

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -139,3 +139,6 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
